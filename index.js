823f8209bbea67321240192d6dc6e6125269a4d7
junblee <topguide82@gmail.com>
2019-10-08 23:48:07 +0900

    get, res

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function handleListening(){
     console.log(`Listening on : http://localhost:${port}`);
 }
 
+function handleHome(req,res) {
+    console.log("Homepage");
+    res.send("Hello Home!");
+}
+
+function handleProfile(req,res){
+    res.send("Profile man");
+}
+
+app.get("/",handleHome);
+
+app.get("/profile",handleProfile);
+
 app.listen(port, handleListening);
