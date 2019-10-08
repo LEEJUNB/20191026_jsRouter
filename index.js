185a7fecf8a5b3439b31e5cdc898e0c144c7d41a
junblee <topguide82@gmail.com>
2019-10-09 01:15:18 +0900

    middleware

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ import express from "express";
 const app = express()
 const port = 4000;
 
-function handleListening(){
+const handleListening= () =>
     console.log(`Listening on : http://localhost:${port}`);
-}
 
-const handleHome = (req,res) => res.send("god Home!");
-
+const handleHome = (req,res) => res.send("god Home!"); // 마지막 함수이기에 next키가 없다.
 const handleProfile = (req,res) => res.send("What'up man");
 
+const betweenHome = (req,res,next) =>  {
+    console.log("I'm between");
+    next();
+};
+
+app.use(betweenHome); // globally middleware
+
 app.get("/",handleHome);
-
 app.get("/profile",handleProfile);
-
 app.listen(port, handleListening);
