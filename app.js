2be1f04e0fdbf24a9879ef4c4956d9dbd7890e0c
junblee <topguide82@gmail.com>
2019-10-12 03:39:07 +0900

    address error that package.json newer file

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const handleProfile = (req,res) => res.send("What'up man");
 
 app.use(cookieParser()); // 쿠키에 유저 정보를 저장한다. 왜냐? session을 다루기위해서
 app.use(bodyParser.json()); // form을 다룰수있어야한다.
-app.use(bodyParser.urlencoded({extended:true})); // form을 다룰수있어야한다.
-app.use(helmet()); // 노드 보안 미들웨어
-app.use(morgan("dev"));  // 모든 것을 로깅(기록)하는 미들웨어
+app.use(bodyParser.urlencoded()); // form을 다룰수있어야한다.
+app.use(morgan("combined"));
+app.use(helmet());
 
-app.get("/",handleHome); // 미들웨어를 끊어보자
+app.get("/",handleHome);
 app.get("/profile",handleProfile);
 
-export default app; // 누군가 내 파일을 불러 올 때 app 객체를 해준다는 의미. app 객체란 앞에서 설정한 app.use, app.get과 같은 큰 덩어리. 
+export default app;
--- init.js
+++ init.js
@@ -1,6 +1,8 @@
-import app from './app'; // app.js모듈을 불러옴
+import app from "./app";
 
-const PORT = 4000;
-const handleListening = () => 
-    console.log(`Listening on : http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
+const port = 3000;
+
+const handleListening= () =>
+    console.log(`Listening on : http://localhost:${port}`);
+
+app.listen(port, handleListening);
