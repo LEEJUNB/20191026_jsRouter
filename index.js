d623e45afeb756a5cf366faab127e26cc69e7845
junblee <topguide82@gmail.com>
2019-10-11 16:31:04 +0900

    helmet,body,cookie-parser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
-import morgan from "morgan";
+import morgan from "morgan"; // 로깅하기_모슨일을했는지 기록 어떤접속이고 어디에접속하는지알수있음
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express()
 const port = 4000;
@@ -10,13 +13,16 @@ const handleListening= () =>
 const handleHome = (req,res) => res.send("god Home!"); // 마지막 함수이기에 next키가 없다.
 const handleProfile = (req,res) => res.send("What'up man");
 
-const betweenHome = (req,res,next) =>  {
-    console.log("I'm between");
-    next();
-};
+app.use(cookieParser()); // 쿠키에 유저 정보를 저장한다. 왜냐? session을 다루기위해서
+app.use(bodyParser.json()); // form을 다룰수있어야한다.
+app.use(bodyParser.urlencoded()); // form을 다룰수있어야한다.
+app.use(morgan("combined"));  // globally middleware
+app.use(helmet());
 
-app.use(betweenHome); // globally middleware
+const middleware = (req,res,next) => { // 미들웨어로 미들웨어 연결을 끊을수있다
+    res.send("NoNoNo");
+}
 
-app.get("/",handleHome);
+app.get("/",middleware,handleHome); // 미들웨어를 끊어보자
 app.get("/profile",handleProfile);
 app.listen(port, handleListening);
